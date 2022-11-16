1bf1a8c313043001f88a120f49839f00502d63bd
Tom Clark <tom@octue.com>
2022-11-16 14:22:40 +0000

    OPS: Update resource settings with names from terraform

--- server/settings.py
+++ server/settings.py
@@ -26,14 +26,17 @@ def get_db_conf():
         }
 
 
-# -------------------------
-# ENVIRONMENT WE OPERATE IN
-# -------------------------
-ENVIRONMENT = "main"
+# -----------------------------------------------------------
+# TERRAFORM VARIABLES USED FOR CONSTRUCTION OF RESOURCE NAMES
+# -----------------------------------------------------------
+TERRAFORM_PROJECT_NAME = "octue-exa"  # 'my-exa-project' in the documentation
+TERRAFORM_RESOURCE_AFFIX = "exa"  # 'my-exa-project' in the documentation
+TERRAFORM_ENVIRONMENT = "main"  # Typically we use a slugified branch name for this
+TERRAFORM_REGION = "europe-west1"
 
 
 # ---------------------------------------------------------------------------
-# GENERIC DJANGO SETTINGS FOR THE TEST APP (scroll down for the good stuff)
+# GENERIC DJANGO SETTINGS FOR THE TEST APP
 # ---------------------------------------------------------------------------
 
 DEBUG = True
@@ -114,13 +117,13 @@ ASGI_APPLICATION = "asgi.application"
 
 # MEDIA FILES
 DEFAULT_FILE_STORAGE = "django_gcp.storage.GoogleCloudMediaStorage"
-GCP_STORAGE_MEDIA = {"bucket_name": "example-media-assets"}
+GCP_STORAGE_MEDIA = {"bucket_name": f"{TERRAFORM_RESOURCE_AFFIX}-{TERRAFORM_ENVIRONMENT}-media-assets"}
 MEDIA_URL = f"https://storage.googleapis.com/{GCP_STORAGE_MEDIA['bucket_name']}/"
 MEDIA_ROOT = "/media/"
 
 # STATIC FILES
 STATICFILES_STORAGE = "django_gcp.storage.GoogleCloudStaticStorage"
-GCP_STORAGE_STATIC = {"bucket_name": "example-static-assets"}
+GCP_STORAGE_STATIC = {"bucket_name": "{TERRAFORM_RESOURCE_AFFIX}-{TERRAFORM_ENVIRONMENT}-static-assets"}
 STATIC_URL = f"https://storage.googleapis.com/{GCP_STORAGE_STATIC['bucket_name']}/"
 STATIC_ROOT = "/static/"
 
@@ -129,15 +132,15 @@ STATIC_ROOT = "/static/"
 # HERE'S HOW TO SET UP TASKS
 # ---------------------------------------------------------------------------
 
-GCP_TASKS_DEFAULT_QUEUE_NAME = f"{ENVIRONMENT}-primary"
+GCP_TASKS_DEFAULT_QUEUE_NAME = f"{TERRAFORM_RESOURCE_AFFIX}-{TERRAFORM_ENVIRONMENT}-primary"
 GCP_TASKS_DELIMITER = "--"
 # This is the domain on which the worker app can receive requests
 # You can use localtunnel to easily create your own public domain to
 # run end-to-end integration tests with a real GCP project
 GCP_TASKS_DOMAIN = "https://outrageous-horny-giraffe.loca.lt"
 GCP_TASKS_EAGER_EXECUTE = False
-GCP_TASKS_REGION = "europe-west1"
-GCP_TASKS_RESOURCE_AFFIX = f"{ENVIRONMENT}"
+GCP_TASKS_REGION = TERRAFORM_REGION
+GCP_TASKS_RESOURCE_AFFIX = f"{TERRAFORM_RESOURCE_AFFIX}-{TERRAFORM_ENVIRONMENT}"
 
 
 # client = google.cloud.storage.Client()
@@ -146,7 +149,6 @@ GCP_TASKS_RESOURCE_AFFIX = f"{ENVIRONMENT}"
 
 
 # DJANGO TWINED
-TWINED_BASE_URL = "https://my-server.com"
-TWINED_DEFAULT_NAMESPACE = "octue"
-TWINED_DEFAULT_PROJECT_NAME = "octue-django-twined-example"
+TWINED_BASE_URL = GCP_TASKS_DOMAIN  # The base url to which ServiceUsageEvents get pushed when services are running. You typically want this to be a worker URL, to consume the event stream.
+TWINED_DEFAULT_NAMESPACE = TERRAFORM_PROJECT_NAME
 TWINED_DEFAULT_TAG = "latest"
